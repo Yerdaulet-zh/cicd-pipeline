db67a5a41f894ab0d1a0d93fd628b4448bb7b75c
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 14:49:24 +0500

    Add nginx & jenkins EC2 SGs

---
+++ vpc/jenkins-sg.tf
@@ -0,0 +1,23 @@
+resource "aws_security_group" "jenkins" {
+  name   = "${local.project_name}-jenkins-sg"
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "jenkins-sg" }
+}
+
+resource "aws_security_group_rule" "jenkins_ingress_from_nginx" {
+  type                     = "ingress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.nginx.id # Links to Nginx
+  security_group_id        = aws_security_group.jenkins.id
+}
+
+resource "aws_security_group_rule" "jenkins_egress_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins.id
+}
---
+++ vpc/nginx-sg.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "nginx" {
+  name   = "${local.project_name}-nginx-sg"
+  vpc_id = aws_vpc.vpc.id
+  tags   = { Name = "nginx-sg" }
+}
+
+resource "aws_security_group_rule" "nginx_ingress_http" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nginx.id
+}
+
+resource "aws_security_group_rule" "nginx_ingress_https" {
+  type              = "ingress"
+  from_port         = 443
+  to_port           = 443
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nginx.id
+}
+
+resource "aws_security_group_rule" "nginx_egress_to_jenkins" {
+  type                     = "egress"
+  from_port                = 8080
+  to_port                  = 8080
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.jenkins.id # Links to Jenkins
+  security_group_id        = aws_security_group.nginx.id
+}
+
+resource "aws_security_group_rule" "nginx_egress_to_internet" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.nginx.id
+}
