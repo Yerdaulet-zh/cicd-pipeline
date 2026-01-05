c98cb2165d4389bf8f045365fc88562d7266ccc6
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 15:14:54 +0500

    Define subnet cidr block

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,5 +1,6 @@
 locals {
-  region       = "eu-south-1"
-  project_name = "cicd-epam-hw"
-  cidr_block   = "10.0.0.0/16"
+  region             = "eu-south-1"
+  project_name       = "cicd-epam-hw"
+  vpc_cidr_block     = "10.0.0.0/16"
+  subnet_public_cidr = "10.0.1.0/24"
 }
