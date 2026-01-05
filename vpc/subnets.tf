84e38ab4f460b8965134a3bb5f8f4b002c479d70
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 14:12:12 +0500

    Create a public subnet

---
+++ vpc/subnets.tf
@@ -0,0 +1,10 @@
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "${local.region}-a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "${local.project_name}-public-subnet"
+  }
+}
