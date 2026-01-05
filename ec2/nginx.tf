e03ae04c95dc1e3eee9f307f6d141b610d7e3fd2
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 15:22:03 +0500

    Add Nginx instance creation resource

---
+++ ec2/nginx.tf
@@ -0,0 +1,22 @@
+resource "aws_instance" "nginx_instance" {
+  ami                    = local.server_ami
+  instance_type          = "t3.micro"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sg_nginx_id]
+  key_name               = var.ssh_key_pair_name
+  private_ip             = local.nginx_private_ip
+
+  root_block_device {
+    volume_type = "gp2"
+    volume_size = 8
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update && sudo apt upgrade -y 
+              EOF
+
+  tags = {
+    Name = "Nginx Server"
+  }
+}
