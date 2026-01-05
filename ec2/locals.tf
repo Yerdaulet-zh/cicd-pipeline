0d1b25de182fa1e01aa85a59ecc6cd6cb0349c6c
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 15:23:19 +0500

    Add jenkins private ip address which will be reserved only for this instance lifetime

--- ec2/locals.tf
+++ ec2/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  server_ami       = "ami-012d712b2b128f2d1"
-  nginx_private_ip = "10.0.1.5"
+  server_ami         = "ami-012d712b2b128f2d1"
+  nginx_private_ip   = "10.0.1.5"
+  jenkins_private_ip = "10.0.1.6"
 }
