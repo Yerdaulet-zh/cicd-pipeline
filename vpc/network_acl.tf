92220a31bfb72630c0ccbc9cc8402e03600dc527
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 14:30:32 +0500

    Add network acl

---
+++ vpc/network_acl.tf
@@ -0,0 +1,43 @@
+resource "aws_network_acl" "acl" {
+  vpc_id = aws_vpc.vpc.id
+  tags = {
+    Name = "${local.project_name}-network-acl"
+  }
+}
+
+resource "aws_network_acl_rule" "network_acl_tcp_ingress" {
+  for_each = {
+    80  = 80
+    443 = 443
+  }
+
+  network_acl_id = aws_network_acl.acl.id
+  rule_number    = each.key
+  egress         = false
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = each.key
+  to_port        = each.value
+}
+
+resource "aws_network_acl_rule" "network_acl_tcp_egress" {
+  for_each = {
+    80  = 80
+    443 = 443
+  }
+
+  network_acl_id = aws_network_acl.acl.id
+  rule_number    = each.key + 1
+  egress         = true
+  protocol       = "tcp"
+  rule_action    = "allow"
+  cidr_block     = "0.0.0.0/0"
+  from_port      = each.key
+  to_port        = each.value
+}
+
+resource "aws_network_acl_association" "network_acl_association" {
+  subnet_id      = aws_subnet.public.id
+  network_acl_id = aws_network_acl.acl.id
+}
