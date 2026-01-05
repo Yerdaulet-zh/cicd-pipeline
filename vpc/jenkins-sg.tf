c3c4d148f19278d1351f07d77bd28c1acd7a4383
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 20:22:57 +0500

    Now the port 8080 publicly available

--- vpc/jenkins-sg.tf
+++ vpc/jenkins-sg.tf
@@ -1,7 +1,18 @@
 resource "aws_security_group" "jenkins" {
   name   = "${local.project_name}-jenkins-sg"
   vpc_id = aws_vpc.vpc.id
-  tags   = { Name = "jenkins-sg" }
+  tags = {
+    Name = "jenkins-sg"
+  }
+}
+
+resource "aws_security_group_rule" "jenkins_ingress_ssh" {
+  type              = "ingress"
+  from_port         = 22
+  to_port           = 22
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins.id
 }
 
 resource "aws_security_group_rule" "jenkins_ingress_from_nginx" {
@@ -11,6 +22,15 @@ resource "aws_security_group_rule" "jenkins_ingress_from_nginx" {
   protocol                 = "tcp"
   source_security_group_id = aws_security_group.nginx.id # Links to Nginx
   security_group_id        = aws_security_group.jenkins.id
+}
+
+resource "aws_security_group_rule" "jenkins_ingress_public" {
+  type              = "ingress"
+  from_port         = 8080
+  to_port           = 8080
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.jenkins.id
 }
 
 resource "aws_security_group_rule" "jenkins_egress_all" {
