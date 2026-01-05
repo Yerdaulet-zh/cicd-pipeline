3de2ad9dbb2458cde91545dd0974a23e63adb109
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-05 15:15:31 +0500

    now cidr param refers to locals

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
+  cidr_block              = local.subnet_public_cidr
   availability_zone       = "${local.region}a"
   map_public_ip_on_launch = true
 
