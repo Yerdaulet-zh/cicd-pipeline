f55e203005aa018c0d4c26849a4902ad76397123
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-06 19:55:41 +0500

    Create two private subnets

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -10,11 +10,13 @@ resource "aws_subnet" "public" {
 }
 
 resource "aws_subnet" "private" {
+  for_each = local.private_subnets_cidrs
+
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = local.subnet_private_cidr
-  availability_zone = "${local.region}a"
+  cidr_block        = each.key
+  availability_zone = "${local.region}${each.value}"
 
   tags = {
-    Name = "${local.project_name}-private-subnet"
+    Name = "${local.project_name}-private-subnets"
   }
 }
