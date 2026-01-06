da722c47ded5c33fdcd93f2ac3db0c3bf1bd1d1a
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-06 16:31:46 +0500

    Create a private subnet

--- vpc/subnets.tf
+++ vpc/subnets.tf
@@ -8,3 +8,13 @@ resource "aws_subnet" "public" {
     Name = "${local.project_name}-public-subnet"
   }
 }
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.vpc.id
+  cidr_block        = local.subnet_private_cidr
+  availability_zone = "${local.region}a"
+
+  tags = {
+    Name = "${local.project_name}-private-subnet"
+  }
+}
