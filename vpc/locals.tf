0f83027a7e2aaafa48ea123d326d8109bae9ccde
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-06 16:32:29 +0500

    Define private subnet cidr address

--- vpc/locals.tf
+++ vpc/locals.tf
@@ -1,6 +1,7 @@
 locals {
-  region             = "eu-south-1"
-  project_name       = "cicd-epam-hw"
-  vpc_cidr_block     = "10.0.0.0/16"
-  subnet_public_cidr = "10.0.1.0/24"
+  region              = "eu-south-1"
+  project_name        = "cicd-epam-hw"
+  vpc_cidr_block      = "10.0.0.0/16"
+  subnet_public_cidr  = "10.0.1.0/24"
+  subnet_private_cidr = "10.0.2.0/24"
 }
