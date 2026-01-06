ea8a2b145b673080070e4f503da01a87221cf8f2
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-06 16:32:13 +0500

    Create route private table and assosiate private subnet

--- vpc/route.tf
+++ vpc/route.tf
@@ -16,3 +16,18 @@ resource "aws_route_table_association" "route_table_association" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.route_table.id
 }
+
+# Private Route Table
+
+resource "aws_route_table" "private_rt" {
+  vpc_id = aws_vpc.vpc.id
+
+  tags = {
+    Name = "${local.project_name}-private-rt"
+  }
+}
+
+resource "aws_route_table_association" "private_rt_association" {
+  subnet_id      = aws_subnet.private.id
+  route_table_id = aws_route_table.private_rt.id
+}
