f7a3e63124a69123dea2d242a1c7e8256fce5f2c
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-07 14:37:30 +0500

    Add SonarQube instance

---
+++ ec2/sonarqube.tf
@@ -0,0 +1,25 @@
+resource "aws_instance" "sonarqube_instance" {
+  ami                    = local.server_ami
+  instance_type          = "t3.medium"
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [var.sg_sonarqube_id]
+  key_name               = var.ssh_key_pair_name
+
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = 20
+    iops        = 3000
+    throughput  = 125
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update && sudo apt upgrade -y
+              # Install dependencies for Postgres and SonarQube
+              sudo apt install -y postgresql-common openjdk-17-jdk
+              EOF
+
+  tags = {
+    Name    = "Sonarqube-Server"
+  }
+}
