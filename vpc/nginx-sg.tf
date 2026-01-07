7dc28a3fef23255a4f7badb18276cbcd2a8a6288
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-07 14:17:51 +0500

    Disable port 80 for internet

--- vpc/nginx-sg.tf
+++ vpc/nginx-sg.tf
@@ -13,14 +13,14 @@ resource "aws_security_group_rule" "nginx_ingress_ssh" {
   security_group_id = aws_security_group.nginx.id
 }
 
-resource "aws_security_group_rule" "nginx_ingress_http" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.nginx.id
-}
+# resource "aws_security_group_rule" "nginx_ingress_http" {
+#   type              = "ingress"
+#   from_port         = 80
+#   to_port           = 80
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_security_group.nginx.id
+# }
 
 resource "aws_security_group_rule" "nginx_ingress_https" {
   type              = "ingress"
