beab8c8bdff9f21e893ae4a078006823377ea7fe
Yerdaulet-zh <yerdaulet.zhumabay@gmail.com>
2026-01-07 19:03:38 +0500

    Specify the SonarQube stable private IP address

--- ec2/sonarqube.tf
+++ ec2/sonarqube.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "sonarqube_instance" {
   instance_type          = "t3.medium"
   subnet_id              = var.subnet_id
   vpc_security_group_ids = [var.sg_sonarqube_id]
+  private_ip             = local.sonarqube_private_ip
   key_name               = var.ssh_key_pair_name
 
   root_block_device {
